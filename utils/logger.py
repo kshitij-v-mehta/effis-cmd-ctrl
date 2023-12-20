af8dca3de6524603c26101f1f5a78b7dd4cd10a4
Kshitij V. Mehta <kshitij-v-mehta@users.noreply.github.com>
2023-12-20 14:36:09 -0500

    reverted logger. doesn't work for some reason, and prints out logger func and line num in output

--- utils/logger.py
+++ utils/logger.py
@@ -1,22 +1,9 @@
 import logging
-from mpi4py import MPI
 
-
-class MyLogger(logging.Logger):
-    def __init__(self, name, level=logging.DEBUG):
-        return super().__init__(name, level)
-
-
-    def info(self, msg, *args, **kwargs):
-        if MPI.COMM_WORLD.Get_rank() == 0:
-            return super().info(msg, *args, **kwargs)
-
-
-logging.setLoggerClass(MyLogger)
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)s | %(asctime)s | %(filename)16s:%(funcName)32s():%(lineno)3s | %(message)s",
-                    datefmt="%Y-%m-%d %H:%M:%S")
 
 logger = logging.getLogger("effis-cmd-ctrl")
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s | %(asctime)s | %(filename)16s:%(funcName)32s():%(lineno)3s | %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
 
 
 if __name__ == '__main__':
