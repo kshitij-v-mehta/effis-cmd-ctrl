4e0c8bfc8ef22abbee34979686a245ff258b1e96
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2023-12-20 12:47:01 -0500

    custom logger to print root messages only

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,22 @@
 import logging
+from mpi4py import MPI
 
 
-logger = logging.getLogger("effis-cmd-ctrl")
+class MyLogger(logging.Logger):
+    def __init__(self, name, level=logging.DEBUG):
+        return super().__init__(name, level)
+
+
+    def info(self, msg, *args, **kwargs):
+        if MPI.COMM_WORLD.Get_rank() == 0:
+            return super().info(msg, *args, **kwargs)
+
+
+logging.setLoggerClass(MyLogger)
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s | %(asctime)s | %(filename)16s:%(funcName)32s():%(lineno)3s | %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S")
+
+logger = logging.getLogger("effis-cmd-ctrl")
 
 
 if __name__ == '__main__':
